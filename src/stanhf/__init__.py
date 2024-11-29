2d5755bf51fb530a138bfa5e0b5f0f70b1178264
Andrew Fowlie <andrew.fowlie@xjtlu.edu.cn>
2024-11-29 12:56:08 +0800

    fix typo

--- src/stanhf/__init__.py
+++ src/stanhf/__init__.py
@@ -3,7 +3,7 @@ Convert histfactory and build and validate Stan program
 =======================================================
 """
 
-from .convert import convert  # noqa: F401
+from .convert import convert, Convert  # noqa: F401
 from .run import install, build, validate  # noqa: F401
 from .cli import cli  # noqa: F401
 from .modifier import Modifier, MODIFIERS  # noqa: F401
