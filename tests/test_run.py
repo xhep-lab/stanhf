0228e3439b4013cc762642dca4fbdd00b0d6d18d
Andrew Fowlie <andrew.fowlie@xjtlu.edu.cn>
2024-12-02 11:53:19 +0800

    add test of target

---
+++ tests/test_run.py
@@ -0,0 +1,22 @@
+"""
+Test Stan target function
+=========================
+"""
+
+import os
+
+import numpy as np
+
+from stanhf import convert, validate
+
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+EXAMPLE = os.path.normpath(os.path.join(CWD, "..", "examples", "example.json"))
+RNG = np.random.default_rng(111)
+
+def test_target():
+    """
+    Validate output from Stan against pyhf
+    """
+    root = convert(EXAMPLE)
+    validate(root, RNG)
