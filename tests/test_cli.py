25b4ecc01800e99e69ddca89bcff23189b6d3baf
Andrew Fowlie <andrew.j.fowlie@gmail.com>
2025-03-05 15:26:25 +0800

    add test of cli

---
+++ tests/test_cli.py
@@ -0,0 +1,19 @@
+"""
+Test stanhf CLI
+===============
+"""
+
+import os
+
+from click.testing import CliRunner
+from stanhf.cli import cli
+
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+EXAMPLE = os.path.normpath(os.path.join(CWD, "..", "examples", "example.json"))
+
+
+def test_cli():
+  runner = CliRunner()
+  result = runner.invoke(cli, [EXAMPLE])
+  assert result.exit_code == 0
