e57bafaad7574063fe0030aca0511be021d28b2f
Andrew Fowlie <andrew.j.fowlie@gmail.com>
2025-03-05 13:40:12 +0800

    tidy exposed functions

--- src/stanhf/__init__.py
+++ src/stanhf/__init__.py
@@ -5,5 +5,3 @@ Convert histfactory and build and validate Stan program
 
 from .convert import Convert  # noqa: F401
 from .run import install  # noqa: F401
-from .cli import cli  # noqa: F401
-from .modifier import Modifier, MODIFIERS  # noqa: F401
